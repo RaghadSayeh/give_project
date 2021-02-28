45662410c45ce573bbc4f834e0b669a8ee7bfc42
RaghadSayeh <raghadsayeh6522@gmail.com>
2021-02-28 20:41:14 +0200

    add some desgin to loginsignup page

--- lib/LoginORSignup.dart
+++ lib/LoginORSignup.dart
@@ -14,36 +14,55 @@ class LoginORSignupState extends State<LoginORSignup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.cyan[200],
-          title: new Text(
-            "إنشاء حساب أو دخول",
-            // textAlign: TextAlign.left,
-            textDirection: TextDirection.ltr,
-          )),
       body: Container(
-        margin: EdgeInsets.all(6.0),
+        color: Colors.cyan[200],
+        // margin: EdgeInsets.all(6.0),
         padding: EdgeInsets.all(6.0),
         child: Center(
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Container(
-            width: 200,
-            height: 80,
+            //  width: 80,
+            height: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Image.asset("assets/images/background_logo.jpg"),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Container(
+            child: Center(
+              child: new Text(
+                "أهلا بك مجددا في عطاء",
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100.0,
+          ),
+          Container(
+            width: 240,
+            height: 70,
             child: RaisedButton(
               onPressed: () {
                 print("إنشاء حساب");
               },
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
+                  borderRadius: BorderRadius.circular(25.0),
                   side: BorderSide(color: Colors.tealAccent[400])),
-              color: Colors.cyan[200],
+              color: Colors.white,
               child: new Text(
                 "إنشاء حساب",
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 18.0),
+                    color: Colors.cyan[200],
+                    fontSize: 19.0),
               ),
             ),
           ),
@@ -54,24 +73,22 @@ class LoginORSignupState extends State<LoginORSignup> {
               width: 200,
               height: 80,
               //  color: Colors.cyan[200],
-              child: RaisedButton(
-                  onPressed: () {
-                    print("تسجيل الدخول");
+              child: GestureDetector(
+                  onTap: () {
                     Navigator.pushReplacement(
                         context,
                         MaterialPageRoute(
                             builder: (context) => new LoginPage()));
                   },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.tealAccent[400])),
-                  color: Colors.cyan[200],
-                  child: new Text(
-                    "تسجيل الدخول",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 18.0),
+                  child: Center(
+                    child: new Text(
+                      "هل لديك حساب مسبقا",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                          color: Colors.white),
+                    ),
                   ))),
         ])),
       ),
