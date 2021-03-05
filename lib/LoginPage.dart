3490f66e9662df741a00977eb98a33b55afcef33
RaghadSayeh <raghadsayeh6522@gmail.com>
2021-03-05 23:08:46 +0200

    add new modifications

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,105 +1,256 @@
 import 'package:flutter/material.dart';
 import 'package:gradient_text/gradient_text.dart';
 
+import 'HomePageSeller.dart';
+
 class LoginPage extends StatefulWidget {
   LoginPageState createState() => LoginPageState();
 }
 
 class LoginPageState extends State<LoginPage> {
+  String logintype;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  TextEditingController _usernameController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+  TextEditingController _nameController = new TextEditingController();
+  String _email;
+  String _password;
+  String _displayName;
+  bool _obsecure = false;
+
   @override
   void initState() {
     super.initState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final emailField = TextField(
-      obscureText: false,
-      style: TextStyle(fontSize: 20.0),
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          hintText: "اسم المستخدم",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final passwordField = TextField(
-      obscureText: true,
-      style: TextStyle(fontSize: 20.0),
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          hintText: "كلمة المرور",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      // color: Colors.cyan[200],
-      color: Color.alphaBlend(Colors.cyan[200], Colors.tealAccent[400]),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
-        child: Text("دخول",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20.0)
-                .copyWith(color: Colors.white, fontWeight: FontWeight.bold)),
+  //input widget
+  Widget _input(
+      Icon icon, String hint, TextEditingController controller, bool obsecure) {
+    return Container(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: TextField(
+        controller: controller,
+        obscureText: obsecure,
+        textAlign: TextAlign.right,
+        style: TextStyle(
+          fontSize: 20,
+        ),
+        decoration: InputDecoration(
+            hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            hintText: hint,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+                width: 2,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+                width: 3,
+              ),
+            ),
+            suffixIcon: Padding(
+              child: IconTheme(
+                data: IconThemeData(color: Theme.of(context).primaryColor),
+                child: icon,
+              ),
+              padding: EdgeInsets.only(left: 10, right: 30),
+            )),
       ),
     );
+  }
 
+  //button widget
+  Widget _button(String text, Color splashColor, Color highlightColor,
+      Color fillColor, Color textColor, void function()) {
+    return RaisedButton(
+      highlightElevation: 0.0,
+      splashColor: splashColor,
+      highlightColor: highlightColor,
+      elevation: 0.0,
+      color: fillColor,
+      shape:
+          RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+      child: Text(
+        text,
+        style: TextStyle(
+            fontWeight: FontWeight.bold, color: textColor, fontSize: 20),
+      ),
+      onPressed: () {
+        function();
+      },
+    );
+  }
+
+  void _loginUser() {
+    _email = _usernameController.text;
+    _password = _passwordController.text;
+    _usernameController.clear();
+    _passwordController.clear();
+    //HomePageSeller
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => new HomePageSeller()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color primary = Theme.of(context).primaryColor;
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-          backgroundColor:
-              Color.alphaBlend(Colors.cyan[200], Colors.tealAccent[400]),
-          title: Directionality(
-            child: Text('تسجيل الدخول'),
-            textDirection: TextDirection.ltr,
-          )),
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+          iconSize: 20.0,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: new Text(
+          "تسجيل الدخول",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: new DecoratedBox(
+        decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40.0), topRight: Radius.circular(40.0)),
+          child: Container(
+            child: ListView(
               children: <Widget>[
                 SizedBox(
-                    height: 150.0,
-                    child: Center(
-                      child: Container(
-                          padding: EdgeInsets.only(top: 4, bottom: 4),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                                colors: [
-                                  Colors.cyan[200],
-                                  Colors.tealAccent[400]
-                                ]),
-                          ),
-                          child: Center(
-                            child: Image.asset(
-                              "assets/images/logo.png",
-                              fit: BoxFit.contain,
-                            ),
-                          )),
-                    )),
-                SizedBox(height: 45.0),
-                emailField,
-                SizedBox(height: 25.0),
-                passwordField,
-                SizedBox(
-                  height: 35.0,
+                  height: 10,
                 ),
-                loginButon,
-                SizedBox(
-                  height: 15.0,
+                SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 140,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              child: Align(
+                                child: Container(
+                                  width: 130,
+                                  height: 130,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Theme.of(context).primaryColor),
+                                ),
+                                alignment: Alignment.center,
+                              ),
+                            ),
+                            Positioned(
+                              child: Container(
+                                child: Text(
+                                  "GIVE",
+                                  style: TextStyle(
+                                    fontSize: 65,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                alignment: Alignment.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 15, top: 40),
+                        child: _input(Icon(Icons.person), "اسم المستخدم",
+                            _usernameController, false),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 15),
+                        child: _input(Icon(Icons.lock), "كلمة المرور",
+                            _passwordController, true),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 15,
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.only(
+                            left: 60,
+                          ),
+                          decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                  width: 2.0,
+                                  style: BorderStyle.solid,
+                                  color: Theme.of(context).primaryColor),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0)),
+                            ),
+                          ),
+                          child: DropdownButtonHideUnderline(
+                            child: new DropdownButton<String>(
+                              value: logintype,
+                              dropdownColor: Colors.white,
+                              //         isExpanded: true,
+                              items: <String>[
+                                'متبرع',
+                                'بائع',
+                                'مسؤول دار أيتام'
+                              ].map((String value) {
+                                return new DropdownMenuItem<String>(
+                                  value: value,
+                                  child: new Text(
+                                    value,
+                                    style: TextStyle(
+                                        color: Theme.of(context).primaryColor,
+                                        fontSize: 20.0),
+                                  ),
+                                );
+                              }).toList(),
+                              onChanged: (valueType) {
+                                setState(() {
+                                  logintype = valueType;
+                                });
+                                print("drop down list changed");
+                                print(valueType);
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: 20,
+                            right: 20,
+                            bottom: MediaQuery.of(context).viewInsets.bottom),
+                        child: Container(
+                          child: _button("دخول", Colors.white, primary, primary,
+                              Colors.white, _loginUser),
+                          height: 50,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
+            height: MediaQuery.of(context).size.height / 1.1,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
           ),
         ),
       ),
